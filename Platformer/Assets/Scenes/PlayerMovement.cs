2d73a61b75d038b3262b194aeca402076acdd763
Kugelpanzer <djolemlad@gmail.com>
2019-09-09 15:12:52 +0200

    test move

---
+++ Platformer/Assets/Scenes/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+
+    float moveHorizontal = Input.GetAxis("Horizontal");
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void PlayerMove()
+    {
+
+        /*if(Input. GetButton("Horizontal"))
+        {
+           
+        }*/
+        /*if (Input.GetAxis("Horizontal")>0)
+        {
+            Debug.Log("kretanje");
+        }
+        */
+        rb.MovePosition(transform.position+new Vector3(Input.GetAxis("Horizontal"),0) * Time.deltaTime);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+    }
+}
