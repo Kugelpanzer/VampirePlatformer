07a798acd85a0bac0d6e9d7aa0fa22846a264ca8
djordjerosic <djordjerosic91@gmail.com>
2019-09-17 15:19:38 +0200

    Gargoyle projectile update

---
+++ VampirePlatformer/Assets/Scripts/GargoyleFireProjectile.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GargoyleFireProjectile : MonoBehaviour
+{
+
+
+    public float attackCooldown = 3;
+    public GameObject projectile;
+    public float projetileSpeed = 10;
+
+    float elapsedTime = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime+= Time.deltaTime;
+        if (elapsedTime >= attackCooldown)
+        {
+            elapsedTime = 0;
+            GameObject clone;
+            clone = Instantiate(projectile, transform.position, transform.rotation);
+            clone.GetComponent<Rigidbody2D>().velocity = transform.TransformDirection(Vector3.left * projetileSpeed);
+        }
+    }
+}
